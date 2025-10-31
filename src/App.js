6dea3823cc74ae1d8d7df8aa87125e4801ab0afa
Chen Guang <ethan.chen.guang@gmail.com>
2025-10-31 18:27:09 +0100

    Added cart visibility state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  let cartContent = null;
+  if (cartIsShown) {
+    // cartContent = <Cart />;
+    cartContent = <div>Cart is shown</div>;
+  }
+
   return (
     <CartProvider>
-      <Header />
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
+      {cartContent}
     </CartProvider>
   );
 }
