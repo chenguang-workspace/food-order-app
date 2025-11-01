de95457b149627b9604749804d650766b9614655
Chen Guang <ethan.chen.guang@gmail.com>
2025-11-01 22:44:29 +0100

    update cart add/remove item functions

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,9 @@
 import { useContext } from 'react';
 
-import Modal from '../UI/Modal';
-import CartItem from './CartItem';
 import CartContext from '../../store/cart-context';
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -11,10 +11,12 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  // remove item from cart
   const cartItemRemoveHandler = (id) => {
     cartCtx.removeItem(id);
   };
 
+  // add item to cart
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 });
   };
